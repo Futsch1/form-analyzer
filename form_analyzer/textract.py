3e39317997bcbd3d6f12de3e7b449f8763e877c8
Florian Fetz <florian.fetz@googlemail.com>
2022-07-30 16:28:21 +0200

    Simplified textract.run_textract() function

--- form_analyzer/textract.py
+++ form_analyzer/textract.py
@@ -2,8 +2,30 @@ import glob
 import json
 import logging
 import os
+import typing
 
 import boto3
+from botocore.client import BaseClient
+
+from form_analyzer import form_analyzer_logger
+
+
+def __upload_to_s3(file_name: str, s3: BaseClient,
+                   s3_bucket_name: str = None,
+                   s3_folder: str = '') -> typing.Dict:
+    s3_file_name = s3_folder + os.path.split(file_name)[1]
+    if 'Contents' not in s3.list_objects(Bucket=s3_bucket_name, Prefix=s3_file_name):
+        form_analyzer_logger.log(logging.INFO, f'Uploading to S3 as {s3_file_name}')
+        s3.upload_file(file_name, s3_bucket_name, s3_file_name)
+    else:
+        form_analyzer_logger.log(logging.DEBUG, f'File {s3_file_name} already on S3')
+
+    return {
+        'S3Object':
+            {'Bucket': s3_bucket_name,
+             'Name': s3_file_name
+             }
+    }
 
 
 def run_textract(folder: str,
@@ -13,8 +35,6 @@ def run_textract(folder: str,
                  use_s3: bool = False,
                  s3_bucket_name: str = None,
                  s3_folder: str = ''):
-    from form_analyzer import form_analyzer_logger
-
     for file_name in sorted(glob.glob(f'{folder}/*.png')):
         if os.path.exists(f'{file_name}.json'):
             form_analyzer_logger.log(logging.DEBUG, f'Skipping {file_name}')
@@ -25,21 +45,7 @@ def run_textract(folder: str,
         if use_s3:
             s3 = boto3.client('s3', region_name=aws_region_name, aws_access_key_id=aws_access_key_id,
                               aws_secret_access_key=aws_secret_access_key)
-
-            s3_file_name = s3_folder + os.path.split(file_name)[1]
-            if 'Contents' not in s3.list_objects(Bucket=s3_bucket_name, Prefix=s3_file_name):
-                form_analyzer_logger.log(logging.INFO, f'Uploading to S3 as {s3_file_name}')
-                s3.upload_file(file_name, s3_bucket_name, s3_file_name)
-            else:
-                form_analyzer_logger.log(logging.DEBUG, f'File {s3_file_name} already on S3')
-
-            document = {
-                'S3Object':
-                    {'Bucket': s3_bucket_name,
-                     'Name': s3_file_name
-                     }
-            }
-
+            document = __upload_to_s3(file_name, s3, s3_bucket_name, s3_folder)
         else:
             with open(file_name, "rb") as image_file:
                 document = {
